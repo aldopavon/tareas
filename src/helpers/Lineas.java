be8adf37601eeb2f766f02111229fb55d8dcaae5
aldo <>
2020-03-12 20:40:32 -0600

    tarea modulo5 clace lineas

---
+++ src/helpers/Lineas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * 
+ */
+public class Lineas extends Formas {
+    public Lineas(){
+        setDibujar("Esta es una linea");
+        setColor("Esta figura es de color verde");
+    }
+    
+}
